5bd4efad5a0f64121b96267d735c5c3392f77194
andranikp2 <andranik.p2@tumo.org>
2024-01-21 15:24:00 +0400

    connect to mongo db

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-var express = require("express");
-const bodyParser = require('body-parser');
-var path = require("path");
-var app = express();
+// var express = require("express");
+// const bodyParser = require('body-parser');
+// var path = require("path");
+// var app = express();
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static('public'));
 
-app.get("/", function(req, res){
- res.sendFile(path.join(__dirname,'./public/form.html'));
+// app.get("/", function(req, res){
+//  res.sendFile(path.join(__dirname,'./public/form.html'));
 
-});
-app.post("/addInfo", function(req, res){
-    let surname = req.body.surname;
-    let age = req.body.age;
-    console.log(surname, age);
-    res.redirect('/')
+// });
+// app.post("/addInfo", function(req, res){
+//     let surname = req.body.surname;
+//     let age = req.body.age;
+//     console.log(surname, age);
+//     res.redirect('/')
     
-   });
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
+//    });
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+
+const mongoose = require('mongoose');
+
+// Replace the connection string with your MongoDB connection string
+const connectionString = " mongodb+srv://andranikp2:and123456@cluster0.60w9mte.mongodb.net/sample_mflix";
+
+
+
+// Connect to MongoDB
+mongoose.connect(connectionString, { useUnifiedTopology: true });
+
+// Check the connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+console.log('Connected to MongoDB!');
+// You can add additional code here for testing or other operations
+// Make sure to close the connection when you're done
+mongoose.connection.close();
 });
 
-
-
+try {
+   const allMovies = await mongoose.connection.db.collection('movies').find().toArray(); // .insertMany(newMovies);
+   
+   console.log('All Movies:', allMovies);
+   } catch (error) {
+   console.error('Error retrieving movies:', error);
+   } finally {
+   mongoose.connection.close();
+   }
+   
+   
+   
+   
